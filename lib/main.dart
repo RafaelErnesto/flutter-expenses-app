9f45db5efb6cc76da22ef4d85ea5dd61851f4e14
Rafael <rflernesto@gmail.com>
2021-07-22 12:02:32 -0400

    feat: format date

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 main() => runApp(ExpensesApp());
 class ExpensesApp extends StatelessWidget{
   @override
@@ -68,7 +68,7 @@ class MyHomePage extends StatelessWidget{
                             ),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date),
                             style: TextStyle(
                               color: Colors.grey
                             )
